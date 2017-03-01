90835747ea9d77b707d0564c78c47617e3ca4236
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-03-01 17:25:12 +0100

    Suggestions for opinion handler

--- client/javascripts/opinion_target.js
+++ client/javascripts/opinion_target.js
@@ -1,4 +1,5 @@
-suggestions = ['The food is tasty', 'The restaurant was very expensive', 'I think the steak was not very good', 'The fish was ok, but I the salad was better', 'The breakfast was delicious!', 'How much does these oranges cost?']
+//suggestions = ['The food is tasty', 'The restaurant was very expensive', 'I think the steak was not very good', 'The fish was ok, but I the salad was better', 'The breakfast was delicious!', 'How much does these oranges cost?']
+var suggestions = [];
 function new_question(question) {
 
 	$('#question').text(question);
@@ -19,7 +20,6 @@ function new_opinion_answer(original, labels) {
 		var original_splitted = original.split(" ");
 		var formatted_text = '';
 		for (i = 0; i < l.length; i++) {
-			console.log(l[i]);
 			if (l[i] == "O") {
 				formatted_text += '<span>' + original_splitted[i] + ' </span>';
 			} else {
@@ -29,8 +29,9 @@ function new_opinion_answer(original, labels) {
 		formatted_texts.push(formatted_text);
 	}
 	$('#question').text('');
-
-	$('#answer_baseline').html(formatted_texts[0]);
+	suggestions_random = get_random_suggestions(suggestions);
+    load_suggestions(suggestions_random);	
+    $('#answer_baseline').html(formatted_texts[0]);
 	$('#answer_embeddings').html(formatted_texts[1]);
 	stop_spinner();
 }
@@ -51,7 +52,8 @@ function submit(input_text) {
 	  }
 	});
 }
-function load_suggestions(){
+function load_suggestions(suggestions){
+	clean()
 	for (var c in suggestions) {
 		$('#suggestions').html($('#suggestions').html() + '<div class="col-md-4 suggestion_box"><div class="panel panel-default suggestion"><div class="panel-body">' + suggestions[c] + ' </div></div></div>');
 	}
@@ -62,14 +64,44 @@ function load_suggestions(){
 	})
 }
 
+function get_random_suggestions(suggestions) {
+	var random_indexes = []
+	while(random_indexes.length < 6){
+	    var random_number = Math.ceil(Math.random()*(suggestions.length-1));
+	    if(random_indexes.indexOf(random_number) > -1) continue;
+	    random_indexes[random_indexes.length] = random_number;
+	}
+
+	suggestions_random = []
+	for (var i in random_indexes) {
+		suggestions_random.push(suggestions[random_indexes[i]]);
+	}
+	return suggestions_random
+}
+function clean() {
+	$('#question').text('');
+	$('#answer_baseline').text('');
+	$('#answer_embeddings').text('');
+	$('#suggestions').text('');
+}
+function refresh() {
+	suggestions_random = get_random_suggestions(suggestions);
+	load_suggestions(suggestions_random);
+}
 
 $(document).ready(function(){
-	load_suggestions();
-
+	$.getJSON("../javascripts/lists/opinion_mining.json", function(json) {
+		suggestions = json.candidates;
+		suggestions_random = get_random_suggestions(suggestions);
+	    load_suggestions(suggestions_random);
+	});
+	
 	$('#search_button').click(function(e){
 		input_text = $('#input_text').val();
 	    if (input_text != '') submit(input_text, $('#project_value').text().toLowerCase());
 	})
-
+	$('#refresh_button').click(function(e){
+		refresh();
+	})
 });
 
